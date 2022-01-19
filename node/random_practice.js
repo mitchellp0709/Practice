3f0cd3d41481971c2bd200956b1bdda87d9ef655
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-01-19 12:18:26 -0500

    insertion sort

--- node/random_practice.js
+++ node/random_practice.js
@@ -14,39 +14,42 @@
 // sumArrayOfNums([2,4,5,8])
 
 
-function bubbleSort(array) {
-  // Make some sort of loop that keeps happening if there were any swaps
-  // that happened this turn
-  let sort = true 
-  while (sort) {
-    sort = false;
-    for (let i = 0; i < array.length-1;i++) {
-      if (array[i + 1] < array[i]) {
-        let small = array[i+1]
-        let big = array[i]
-        array[i] = small
-        array[i + 1] = big
-        small = null
-        big = null
-        sort = true 
-      }
+// function bubbleSort(array) {
+//   // Make some sort of loop that keeps happening if there were any swaps
+//   // that happened this turn
+//   let sort = true 
+//   while (sort) {
+//     sort = false;
+//     for (let i = 0; i < array.length-1;i++) {
+//       if (array[i + 1] < array[i]) {
+//         let small = array[i+1]
+//         let big = array[i]
+//         array[i] = small
+//         array[i + 1] = big
+//         small = null
+//         big = null
+//         sort = true 
+//       }
+//     }
+//   }
+//   return array;
+// }
+
+// console.log(bubbleSort([12, 6, 3, 7, 13, 8,0,70,100,-1]));
+
+
+function insertionSort(array) {
+  
+  for (let i = 1; i < array.length; i++) {
+    let value = array[i] 
+    let j = i - 1; 
+    while ((j > -1) && (value < array[j])) {
+      array[j + 1] = array[j]
+      j--
     }
+    array[j+1]=value
   }
 
-  // reset swapHappened to false each time so we can detect if a swap
-  // happened in this specific iteration.
-
-  // Make another loop (inside the first one) to go through one
-  // round of swapping from the start of the list to the end
-
-  // Inside inner loop:
-  // compare each pair of elements near each other
-  // swap them if the bigger one was at a lower index.
-
-  // Make sure to keep track of whether a swap happened!
-
-  // After both loops have exited, remember to return the array
-  return array;
+  return array
 }
-
-console.log(bubbleSort([12, 6, 3, 7, 13, 8,0,70,100,-1]));
+console.log(insertionSort([12, 6, 3, 7, 13, 8, 0, 70, 100, -1]));
