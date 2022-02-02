f141041f927dab3bd6c316951be5356946a34f21
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-02-02 10:46:46 -0500

    more practice

---
+++ node/typescriptPractice.ts
@@ -0,0 +1,38 @@
+// const firstName: string = "Mitchell"
+
+// const age: number = 50
+
+
+// const increment = (counterVal:number) => {
+//   return counterVal +1
+// }
+
+// const counter: number = increment(10)
+
+// console.log(counter)
+
+// let num: number = 5;
+// console.log(num)
+// num = 6;
+// console.log(num)
+
+
+// const bool: boolean = false;
+// console.log(bool)
+
+
+/////////////////////////////
+// Expressions
+/////////////////////////////
+
+let num: number 
+num = 5 + 5+5
+console.log(num)
+
+let myString: string
+
+//concatenation
+myString = "five" + "cheese"
+console.log(myString)
+myString = `${num} cheese`
+console.log(myString)
