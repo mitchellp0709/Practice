e4b1a21dda77a68d8a7689694cb0cde741ef8728
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-02-03 14:34:32 -0500

    scratch

---
+++ node/scratch.js
@@ -0,0 +1,13 @@
+const ingredients = []
+
+//ingredients = ["1", "kg", "chicken"]
+
+ingredients.push("1")
+ingredients.push("kg")
+ingredients.push("chicken")
+console.log(ingredients)
+console.log(ingredients.join(' '))
+
+const final = []
+final.push(ingredients.join(' '))
+console.log(final)
