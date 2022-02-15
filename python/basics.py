ea2ddeb95e6fe73867200fcfe49a3160c07bca6a
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-02-15 15:48:19 -0500

    python refresher

--- python/basics.py
+++ python/basics.py
@@ -9,3 +9,29 @@ elif (num > 5 ):
   print("num is greater than 5")
 else:
   print("i dont even know")
+  
+
+counter = 0
+
+# while (counter<10):
+#   print(counter)
+#   if(counter%2==0):
+#     print("it is even")
+#   if(counter % 3 == 0):
+#     print("meow")
+#   counter+=1
+  
+  
+my_list = [1,2,3]
+  
+print(my_list)
+
+my_directory = {
+  "cheese": "gouda",
+  "bread":"rye",
+  "number":5
+}
+
+print(my_directory)
+print (my_directory["cheese"])
+print(my_directory["number"])
