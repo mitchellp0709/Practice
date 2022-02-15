78624bea8d116846e541b7c1c2a89aa77746eb9f
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-02-15 15:42:41 -0500

    python

---
+++ python/basics.py
@@ -0,0 +1,11 @@
+num = -9
+
+print(f"the value is {num}")
+
+
+if (num > 3):
+  print("num is greater than 3")
+elif (num > 5 ):
+  print("num is greater than 5")
+else:
+  print("i dont even know")
