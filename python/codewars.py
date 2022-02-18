4f77b8abb46e4f90a926c63784ba944aefe2bc55
Mitchell Paoletti <mitchellp0709@gmail.com>
2022-02-17 19:00:49 -0500

    cw

--- python/codewars.py
+++ python/codewars.py
@@ -15,25 +15,42 @@
 # x = [int(a) for a in str(testing)]
 # print(x)
 
-def narcissistic( value ):
+# def narcissistic( value ):
   
-  val = [int(a) for a in str(value)]
-  length = len(val)
-  print(val)
-  new_value = map(lambda x:(x**length),val)
-  new_value=tuple(new_value)
-  sum=0
-  for x in new_value:
-    sum+=x
-  if(sum==value):
-    return True
-  else: return False
+#   val = [int(a) for a in str(value)]
+#   length = len(val)
+#   print(val)
+#   new_value = map(lambda x:(x**length),val)
+#   new_value=tuple(new_value)
+#   sum=0
+#   for x in new_value:
+#     sum+=x
+#   if(sum==value):
+#     return True
+#   else: return False
   
 
 
-narcissistic(153)
+# narcissistic(153)
 
 
-# numbers = (1,2,3,4)
-# result=map(lambda x: x*x,numbers)
-# print(result)
+#https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/python
+
+def find_outliers(integers):
+  counter = 0
+  evens = 0
+  while(counter<=2):
+    if(integers[counter]%2==0):
+      evens+=1
+    counter+=1
+  
+  if(evens>1):
+    for x in integers:
+      if(x%2!=0):
+        return x
+  else: 
+    for x in integers:
+      if(x%2==0):
+        return x
+
+print(find_outliers([1,1,4]))
